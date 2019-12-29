48c1a85637598afde5399092c3ab64b896e63dcd
joe11093 <joe11093@users.noreply.github.com>
2019-12-29 21:13:10 +0100

    added echo for the source of the data

--- index.php
+++ index.php
@@ -31,6 +31,7 @@ function serveFile($search_term){
 		//serve from cache
 		$path = "cache/jsonCache/".$search_term.".json";
 		$str_json = file_get_contents($path);
+		echo "Fetching from Cache.";
 		echo $str_json;
 	}
 	else{
@@ -43,6 +44,7 @@ function serveFile($search_term){
 		$extract = removeEmptyLines($extract);
 		extractData($extract);
 		separateRelationsByType();
+		echo "Fetching from JDM.";
 		echo json_encode($obj);
 		saveToFile(json_encode($obj), "cache/jsonCache/".$term['name'].".json");
 	}
