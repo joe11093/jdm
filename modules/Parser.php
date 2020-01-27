41fc58aa8649eca8fe8d37020b5be169f51b0c23
anonbnr <bashar.nabil.rima@gmail.com>
2020-01-27 10:24:30 +0100

    changed includes to require_once to avoid redeclaration of classes

--- modules/Parser.php
+++ modules/Parser.php
@@ -1,6 +1,6 @@
 <?php
-include_once(dirname(__FILE__, 2) . '/classLoader.php');
-include_once(dirname(__FILE__, 2) . '/utility.php');
+require_once(dirname(__FILE__, 2) . '/classLoader.php');
+require_once(dirname(__FILE__, 2) . '/utility.php');
 
 class Parser
 {
